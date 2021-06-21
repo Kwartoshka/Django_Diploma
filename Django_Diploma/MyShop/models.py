c4a4c3741d320f0663c5641b1aabc7fe3d294cb0
Andrey Zaytsev <and.zaytsev9@gmail.com>
2021-06-21 18:58:57 +0300

    initial

---
+++ Django_Diploma/MyShop/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class StatusChoices(models.TextChoices):
+    NEW = 'NEW', 'Новый'
+    IN_PROGRESS = 'IN_PROGRESS', 'В процессе'
+    DONE = 'DONE', 'Выполнен'
+
+class Product(models.Model):
+
+    name = models.CharField()
+    description = models.TextField(default='')
+    price = models.IntegerField(default=0)
+    creation_date = models.DateField(
+        auto_now_add=True
+    )
+    updating_date = models.DateField(default=creation_date)
+
+
+class ProductReview(models.Model):
+
+    author_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    text = models.TextField(default='')
+    mark = models.IntegerField()
+    creation_date = models.DateField(
+        auto_now_add=True
+    )
+    updating_date = models.DateField(default=creation_date)
+
+
+class Order(models.Model):
+
+    author_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    positions =
+    status =  models.TextField(default=StatusChoices.NEW, choices=StatusChoices.choices)
+    order_sum = models.IntegerField()
+    creation_date = models.DateField(
+        auto_now_add=True
+    )
+    updating_date = models.DateField(default=creation_date)
+
+
+class Collection(models.Model):
+
+    title = models.CharField()
+    text = models.TextField(default='')
+    products = models.ManyToManyField(Product)
+    creation_date = models.DateField(
+        auto_now_add=True
+    )
+    updating_date = models.DateField(default=creation_date)
+
+
+class OrderProduct(models.Model):
+    order_id = 
