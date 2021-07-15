6d8a506e5ab46c17cf5e7bed3d38d5c9ee89a8f5
JobingJob <JobingJob27@gmail.com>
2021-07-15 13:01:18 +0300

    BaseTable created

--- Django_Diploma/MyShop/models.py
+++ Django_Diploma/MyShop/models.py
@@ -11,51 +11,51 @@ class StatusChoices(models.TextChoices):
     DONE = 'DONE', 'Выполнен'
 
 
-class Product(models.Model):
+class BaseTable(models.Model):
+    creation_date = models.DateField(auto_now_add=True)
+    updating_date = models.DateField(null=True, default=None)
+
+    class Meta:
+        abstract = True
+
+
+class Product(BaseTable):
 
     name = models.CharField(max_length=128)
     description = models.TextField(default='')
     price = models.PositiveIntegerField(default=0)
-    creation_date = models.DateField(auto_now_add=True)
-    updating_date = models.DateField(null=True, default=None)
 
     def __str__(self):
         return f'{self.name} ({self.price} руб.)'
 
 
-class ProductReview(models.Model):
+class ProductReview(BaseTable):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     text = models.TextField(default='')
     mark = models.IntegerField(validators=[
             MaxValueValidator(5),
             MinValueValidator(1)])
-    creation_date = models.DateField(auto_now_add=True)
-    updating_date = models.DateField(null=True, default=None)
 
     def __str__(self):
         return f'Отзыв {self.author_id} на {self.product}'
 
 
-class Order(models.Model):
+class Order(BaseTable):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     positions = models.ManyToManyField('Position', related_name='orders', through='OrderPosition')
     status = models.TextField(default=StatusChoices.NEW, choices=StatusChoices.choices)
     order_sum = models.IntegerField(null=True)
-    creation_date = models.DateField(auto_now_add=True)
-    updating_date = models.DateField(null=True, default=None)
 
     def __str__(self):
         return f'заказ {self.id} от {self.author} на {self.order_sum} рублей ({self.status})'
 
 
-class Collection(models.Model):
+class Collection(BaseTable):
 
     title = models.CharField(max_length=128)
     text = models.TextField(default='')
     products = models.ManyToManyField(Product, related_name='collections')
-    creation_date = models.DateField(auto_now_add=True)
-    updating_date = models.DateField(null=True, default=None)
 
     def __str__(self):
         return self.title
